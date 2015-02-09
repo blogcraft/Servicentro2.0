e8cda1daba2f08762b49f8ccb5528bacf1135162
Gerardo Buenrostro González <blogcraft@gmail.com>
2015-02-09 00:37:19 -0300

    Soporte para AngularJS Controllers y Directives

---
+++ Servicentro2.0/Scripts/Controllers/app.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    var app = angular.module("servicentro", []);
+
+    app.controller("panelController", function () {
+        this.tab = 0;
+
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+
+    app.directive("menuPanel", function () {
+        return {
+            restrict: "E",
+            templateUrl: "menu-panel.html"
+        }
+    });
+
+    app.directive("menuSitio", function () {
+        return {
+            restrict: "E",
+            templateUrl: "menu-sitio.html"
+        }
+    })
+
+    app.directive("footerSitio", function () {
+        return {
+            restrict: "E",
+            templateUrl: "footer-sitio.html"
+        }
+    });
+})();
