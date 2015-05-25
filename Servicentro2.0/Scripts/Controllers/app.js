8a5f83993946fcb79cfbae1ef624d5cb5bc93e16
Gerardo Buenrostro González <blogcraft@gmail.com>
2015-05-24 23:12:51 -0400

    Se agrega la galeria y las letras, falta terminar

--- Servicentro2.0/Scripts/Controllers/app.js
+++ Servicentro2.0/Scripts/Controllers/app.js
@@ -13,6 +13,64 @@
         };
     });
 
+    var albums = [
+        {
+            id: 1,
+            nombre: "Hallel",
+            ano: 2014,
+            mes: 9,
+            ciudad: "Franca",
+            pais: "Brasil",
+            fotos: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17"]
+        }
+        , {
+            id: 2,
+            nombre: "Hallel",
+            ano: 2014,
+            mes: 9,
+            ciudad: "Franca",
+            pais: "Brasil",
+            fotos: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17"]
+        }
+    ]
+
+    app.controller("albumsController", function () {
+        this.albums = albums;
+    });
+
+    app.directive('baguetteBox', ['$timeout', function ($timeout) {
+        return {
+            restrict: "AC",
+            link: function ($scope, $elm, $attrs) {
+                $timeout(function () {
+                    baguetteBox.run('.baguetteBox'+$scope.$eval($attrs.albumid), {
+                        captions: true, // true|false - Display image captions
+                        buttons: 'auto', // 'auto'|true|false - Display buttons
+                        async: true, // true|false - Load files asynchronously
+                        preload: 2, // [number] - How many files should be preloaded from current image
+                        animation: 'slideIn'
+                    });
+                });
+            }
+        };
+    }]);
+
+    app.directive('lyrics', function () {
+        return {
+            restrict: "C",
+            link: function ($scope, $elm) {
+                $(".letra").hide();
+                $('.nombre').click(function (event) {
+                    event.preventDefault();
+                    $(this).siblings().find('.letra:visible').siblings().find("i").toggleClass("fa-minus-square fa-plus-square");
+                    $(this).siblings().find('.letra').slideUp('slow');
+                    $('i', this).toggleClass("fa-minus-square fa-plus-square");
+                    $('.letra', this).toggle('slow');
+                });
+            }
+        };
+    });
+
     app.directive("menuPanel", function () {
         return {
             restrict: "E",
@@ -33,4 +91,5 @@
             templateUrl: "footer-sitio.html"
         }
     });
+
 })();
